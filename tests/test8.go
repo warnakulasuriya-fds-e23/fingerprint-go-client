d6479ccc90313795539d1b12b6cdbc3e0dfe244e
warnakulasuriya-fds-e23 <fdswarnakulasuriya@gmail.com>
2025-06-24 18:42:08 +0530

    edited Test8 for time measurement

--- tests/test8.go
+++ tests/test8.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"fmt"
 	"log"
+	"time"
 
 	httprequests "github.com/warnakulasuriya-fds-e23/fingerprint-go-client/httpRequests"
 )
@@ -10,9 +11,12 @@ import (
 func Test8() {
 	client := httprequests.NewHttpClientImpl()
 	var NewEntryImagePath string = "/home/dheera/FingerPrintDatabases/veryLargePNGDataset/DB3_B107_1.png"
+	t := time.Now()
 	message, err := client.EnrollTemplateFilesMethod(NewEntryImagePath, "testuser1")
+	duration := time.Since(t)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	fmt.Println(message)
+	fmt.Println("elapsed time : ", duration)
 }
