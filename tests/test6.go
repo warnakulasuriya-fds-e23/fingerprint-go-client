9a92f3ca6a562414f1e9e3a3df225f9e0cc0b781
warnakulasuriya-fds-e23 <fdswarnakulasuriya@gmail.com>
2025-06-24 18:46:01 +0530

    refactored test 6 and test 7

--- tests/test6.go
+++ tests/test6.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	httprequests "github.com/warnakulasuriya-fds-e23/fingerprint-go-client/httpRequests"
-	"github.com/warnakulasuriya-fds-e23/fingerprint-go-sdk/core"
 )
 
 func Test6() {
@@ -15,33 +14,18 @@ func Test6() {
 	var matchingImagePath string = probeImagePath
 	var nonMatchingImagePath string = "/home/dheera/FingerPrintDatabases/veryLargePNGDataset/DB1_B102_1.png"
 
-	var fingerprintImagesDirectoryPath string = "/home/dheera/FingerPrintDatabases/veryLargePNGDataset/"
-	var cborDirectoryPath = "/home/dheera/FingerPrintDatabases/cborDirectory2ForGo/"
-
-	sdk, err := core.NewSDKCore(fingerprintImagesDirectoryPath, cborDirectoryPath)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	probe, err := sdk.Extract(probeImagePath)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	matchingCandidate, err := sdk.Extract(matchingImagePath)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	nonMatchingCandidate, err := sdk.Extract(nonMatchingImagePath)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
 	t := time.Now()
-	isMatch1 := client.MatchTemplates(probe, matchingCandidate)
+	isMatch1, err := client.MatchTemplatesFilesMethod(probeImagePath, matchingImagePath)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	firstduration := time.Since(t)
 
 	t = time.Now()
-	isMatch2 := client.MatchTemplates(probe, nonMatchingCandidate)
+	isMatch2, err := client.MatchTemplatesFilesMethod(probeImagePath, nonMatchingImagePath)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	secondduration := time.Since(t)
 
 	fmt.Println("probe and matching Candidate: ", isMatch1)
--- tests/test7.go
+++ tests/test7.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	httprequests "github.com/warnakulasuriya-fds-e23/fingerprint-go-client/httpRequests"
-	"github.com/warnakulasuriya-fds-e23/fingerprint-go-sdk/core"
 )
 
 func Test7() {
@@ -14,21 +13,11 @@ func Test7() {
 	client := httprequests.NewHttpClientImpl()
 	var probeImagePath string = "/home/dheera/FingerPrintDatabases/veryLargePNGDataset/DB3_B107_1.png"
 
-	var fingerprintImagesDirectoryPath string = "/home/dheera/FingerPrintDatabases/veryLargePNGDataset/"
-	var cborDirectoryPath = "/home/dheera/FingerPrintDatabases/cborDirectory2ForGo/"
-
-	sdk, err := core.NewSDKCore(fingerprintImagesDirectoryPath, cborDirectoryPath)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	probe, err := sdk.Extract(probeImagePath)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
 	t := time.Now()
-	isMatched, discoveredId := client.IdentifyTemplate(probe)
+	isMatched, discoveredId, err := client.IdentifyTemplateFilesMethod(probeImagePath)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	duration := time.Since(t)
 	fmt.Printf("Is Matched: %t \n Discovered Id: %s\n", isMatched, discoveredId)
 	fmt.Println("Elapsed Time: ", duration)
